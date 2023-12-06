288b5f3ebdf34e7a41374dd97b58c552754d28d0
Pham Minh Triet <vodanhtinh203@gmail.com>
2023-12-06 11:16:13 +0700

    chỉnh sửa header + thay favicon

--- src/NavigationHeader.js
+++ src/NavigationHeader.js
@@ -15,16 +15,20 @@ function SearchBar() {
 export default function NavigationHeader() {
     return (
         <div className={styles.Header} >
-            <img className={styles.Icon}
-                src='https://cdn.haitrieu.com/wp-content/uploads/2021/10/Logo-DH-Cong-Nghe-Thong-Tin-UIT.png'
-                alt='UIT Icon' />
-            <p className={styles.ShopName}>UIT SHOP</p>
-            <SearchBar />
-            <ul className={styles.HeaderRemainingItems}>
-                <li><i className="fa-solid fa-clipboard iconSize"></i><span>Đơn hàng</span></li>
-                <li><i className="fa-solid fa-cart-shopping iconSize"></i><span>Giỏ hàng</span></li>
-                <li><i className="fa-solid fa-user iconSize"></i><span>Đăng nhập</span></li>
-            </ul>
+            <div className={styles.HeaderPart} style={{ flex: 2, justifyContent: 'flex-start' }} >
+                <img className={styles.Icon}
+                    src='https://cdn.haitrieu.com/wp-content/uploads/2021/10/Logo-DH-Cong-Nghe-Thong-Tin-UIT.png'
+                    alt='UIT Icon' />
+                <p className={styles.ShopName}>UIT SHOP</p>
+                <SearchBar />
+            </div>
+            <div className={styles.HeaderPart} style={{ flex: 1, }}>
+                <ul className={styles.HeaderRemainingItems}>
+                    <li><i className="fa-solid fa-clipboard iconSize"></i><span>Đơn hàng</span></li>
+                    <li><i className="fa-solid fa-cart-shopping iconSize"></i><span>Giỏ hàng</span></li>
+                    <li><i className="fa-solid fa-user iconSize"></i><span>Đăng nhập</span></li>
+                </ul>
+            </div>
         </div>
     )
 }
