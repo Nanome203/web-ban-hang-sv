bf618997e94ab27dd3c2476dfd17397b9a38859e
Pham Minh Triet <vodanhtinh203@gmail.com>
2023-12-14 14:04:30 +0700

    thêm "chưa có đánh giá"

--- src/components/OneProductFrame.jsx
+++ src/components/OneProductFrame.jsx
@@ -9,6 +9,7 @@ function OneProductFrame(props) {
         style={{ width: '200px', height: '200px', objectFit: 'contain' }} />
       <p className={styles.productName}>{props.name}</p>
       <p className={styles.price}>{props.price} VND</p>
+      <p className={styles.rating}>Chưa có đánh giá</p>
 
     </div>
   )
