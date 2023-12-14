04c29c46f793f4b773a2a8fe6dfef23c6a890ed9
Pham Minh Triet <vodanhtinh203@gmail.com>
2023-12-14 14:18:55 +0700

    add footer placeholder

--- src/layout/SideBarLayout.jsx
+++ src/layout/SideBarLayout.jsx
@@ -3,12 +3,15 @@ import SideBar from '../components/SideBar'
 import { Outlet } from 'react-router-dom'
 function SideBarLayout() {
   return (
-    <div id='pageBody' style={{ display: 'flex' }}>
-      <SideBar />
-      <div id='contentBody' style={{ width: '80%', height: '100%' }}>
-        <Outlet />
+    <>
+      <div id='pageBody' style={{ display: 'flex' }}>
+        <SideBar />
+        <div id='contentBody' style={{ width: '80%', height: '100%' }}>
+          <Outlet />
+        </div>
       </div>
-    </div>
+      <footer></footer>
+    </>
   )
 }
 
